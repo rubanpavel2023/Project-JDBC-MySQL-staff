2227044309f9ebe2783e5c825cdf91a713625371
PavelRuban <rubanpavel@outlook.com>
2025-01-11 05:02:16 +0200

    Commit#2 /11012025

---
+++ src/main/java/org/example/app/constants/Constants.java
@@ -0,0 +1,7 @@
+package org.example.app.constants;
+
+public class Constants {
+    public static final String TABLE_EMPLOYEES = "employees";
+    public static final String DATA_INSERT_MSG = "Employee created successfully";
+
+}
---
+++ src/main/java/org/example/app/repository/EmployeeCreateRepository.java
@@ -0,0 +1,35 @@
+package org.example.app.repository;
+
+import org.example.app.constants.Constants;
+import org.example.app.database.DBConn;
+import org.example.app.entity.Employee;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EmployeeCreateRepository {
+    public String createEmployees(Employee employee) {
+
+        String sql = "INSERT INTO" + Constants.TABLE_EMPLOYEES +
+                " (first_Name, last_Name, telephone, email, id_Company, company_Type) " +
+                "VALUES(?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DBConn.connect()) {
+            assert conn != null;
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, employee.getFirstName());
+                pstmt.setString(2, employee.getLastName());
+                pstmt.setString(3, employee.getTelephone());
+                pstmt.setString(4, employee.getEmail());
+                pstmt.setInt(5, employee.getIdCompany());
+                pstmt.setString(6, employee.getCompanyType());
+                pstmt.executeUpdate();
+                return Constants.DATA_INSERT_MSG;
+            }
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
+
+
+    }
+}
